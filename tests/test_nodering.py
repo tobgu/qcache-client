e3e156219634ed25a68168384e199282e873871f
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2015-10-14 07:55:54 +0200

    Improved node ring tests a little

--- tests/test_nodering.py
+++ tests/test_nodering.py
@@ -9,34 +9,40 @@ def test_distribution():
     node_weights = {'aaa': 1, 'bbb': 2, 'ccc': 1, 'ddd': 1, 'eee': 1}
 
     ring = NodeRing(node_weights.keys(), node_weights)
-    distribution = defaultdict(lambda: 1)
+    distribution = defaultdict(list)
     for s in random_strings:
-        distribution[ring.get_node(s)] += 1
+        distribution[ring.get_node(s)].append(s)
 
-    assert 9000 < distribution['aaa'] < 11000
-    assert 18000 < distribution['bbb'] < 22000
-    assert 9000 < distribution['ccc'] < 11000
-    assert 9000 < distribution['ddd'] < 11000
-    assert 9000 < distribution['eee'] < 11000
+    assert 9000 < len(distribution['aaa']) < 11000
+    assert 18000 < len(distribution['bbb']) < 22000
+    assert 9000 < len(distribution['ccc']) < 11000
+    assert 9000 < len(distribution['ddd']) < 11000
+    assert 9000 < len(distribution['eee']) < 11000
 
-    # Drop a node
+    # Drop a node, the new distributions should be super sets of the previous.
+    # Existing nodes should not move.
     ring.remove_node('aaa')
 
-    removed_distribution = defaultdict(lambda: 1)
+    removed_distribution = defaultdict(list)
     for s in random_strings:
-        removed_distribution[ring.get_node(s)] += 1
+        removed_distribution[ring.get_node(s)].append(s)
 
-    assert 22000 < removed_distribution['bbb'] < 26000
-    assert 11000 < removed_distribution['ccc'] < 13000
-    assert 11000 < removed_distribution['ddd'] < 13000
-    assert 11000 < removed_distribution['eee'] < 13000
+    assert 22000 < len(removed_distribution['bbb']) < 26000
+    assert 11000 < len(removed_distribution['ccc']) < 13000
+    assert 11000 < len(removed_distribution['ddd']) < 13000
+    assert 11000 < len(removed_distribution['eee']) < 13000
+
+    assert set(distribution['bbb']) <= set(removed_distribution['bbb'])
+    assert set(distribution['ccc']) <= set(removed_distribution['ccc'])
+    assert set(distribution['ddd']) <= set(removed_distribution['ddd'])
+    assert set(distribution['eee']) <= set(removed_distribution['eee'])
 
     # Add the node back again and check that the distribution goes back to previous
     ring.add_node('aaa', weight=1)
 
-    added_distribution = defaultdict(lambda: 1)
+    added_distribution = defaultdict(list)
     for s in random_strings:
-        added_distribution[ring.get_node(s)] += 1
+        added_distribution[ring.get_node(s)].append(s)
 
     assert distribution['aaa'] == added_distribution['aaa']
     assert distribution['bbb'] == added_distribution['bbb']
