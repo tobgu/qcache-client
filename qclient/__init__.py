b87d55cd572e16be5d5bf18fbb4c9be3e472f1bf
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2015-10-15 22:33:52 +0200

    First skeleton of the client

--- qclient/__init__.py
+++ qclient/__init__.py
@@ -1,1 +1,43 @@
+import json
+import requests
+from qclient.node_ring import NodeRing
+
 __version__ = "0.1.0"
+
+RETRY_COUNT = 3
+
+
+class QClient(object):
+    def __init__(self, node_list):
+        self.node_ring = NodeRing(node + '/' if not node.endswith('/') else node for node in node_list)
+        self.unreachable_nodes = []
+
+    def query(self, key, q, load_fn, load_fn_kwargs=None, content_type='text/csv', accept='application/json'):
+        json_q = json.loads(q)
+        for _ in range(RETRY_COUNT):
+            node = self.node_ring.get_node(key)
+
+            # TODO: Connection error
+            key_url = node + '/dataset/' + key
+            response = requests.get(key_url, params={'q': json_q}, headers={'Accept': accept})
+            if response.status_code == 200:
+                return response.content
+
+            if response.status_code == 404:
+                response = requests.post(key_url, headers={'Content-type': content_type},
+                                         data=load_fn(**load_fn_kwargs))
+                if response.status_code != 201:
+                    raise Exception('Unable to create dataset, status code {status_code}'.format(
+                        status_code=response.status_code))
+            elif response.status_code == 400:
+                # TODO
+                pass
+            elif response.status_code == 406:
+                # TODO
+                pass
+            elif response.status_code == 415:
+                # TODO
+                pass
+            else:
+                # TODO
+                pass
