422bd7bea5ed705362bcc4a663087cc0960fbc5e
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2015-12-14 22:45:14 +0100

    Relax required requests version

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
         # eg: "keyword1", "keyword2", "keyword3",
     ],
     install_requires=[
-        "requests==2.8.1"
+        "requests>=2.7.0"
     ],
     extras_require={
         # eg: 'rst': ["docutils>=0.11"],
